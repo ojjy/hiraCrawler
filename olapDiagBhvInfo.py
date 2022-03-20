8bb3d27d96ed54dc3f08507fcc245efef6dc1a2b
오지구영(ojjy90) <46406557+ojjy@users.noreply.github.com>
2022-03-20 21:32:23 +0900

    Update olapDiagBhvInfo.py

--- olapDiagBhvInfo.py
+++ olapDiagBhvInfo.py
@@ -1,7 +1,6 @@
 """
+진료행위
 http://opendata.hira.or.kr/op/opc/olapDiagBhvInfo.do
-진료행위(검사/수술 등) 통계
-요양기관종별
 """
 import os.path
 import pandas as pd
@@ -10,26 +9,33 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
 
+import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def add_lst(code):
         global failed_lst
         failed_lst.append(code)
 
-class DiagBhvInfoCrawler(object):
+class OpendataCrawler(object):
     def __init__(self, url, md_code, data_btn ,directory , by):
         self.url = url
         self.md_code = md_code
         self.data_btn = data_btn
+        #self.count = count
         self.directory = directory
         self.by = by
 
 
     def crawl_data(self):
         url = 'http://opendata.hira.or.kr/op/opc/olapDiagBhvInfo.do'
-        chromedriver = './chromedriver'
-        driver = webdriver.Chrome(chromedriver)
+        download_folder_fullpath = os.path.join(os.getcwd(), "downloads")
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {'download.default_directory': download_folder_fullpath}
+        chrome_options.add_experimental_option('prefs', prefs)
+        chrome_options.headless = False
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         driver.get(url)
 
         for code in self.md_code:
@@ -37,31 +43,30 @@ class DiagBhvInfoCrawler(object):
                 driver.get(url)
                 # main : 메인 창
                 main = driver.current_window_handle
-                driver.find_element_by_xpath('//*[@id="searchPopup"]').click()
+                driver.find_element(By.XPATH, '//*[@id="searchPopup"]').click()
 
                 # popup : 팝업 창
                 popup = driver.window_handles
-                # driver.switch_to_window(popup.pop())
                 driver.switch_to.window(popup.pop())
                 driver.implicitly_wait(20)
 
                 # 코드명으로 데이터 조회
                 try:
-                    driver.find_element_by_xpath('//*[@id="searchWrd1"]').send_keys(code)
+                    driver.find_element(By.XPATH, '//*[@id="searchWrd1"]').send_keys(code)
                 except Exception as e:
                     failed_lst.append(code)
 
                 # 진료행위명칭 클릭
-                driver.find_element_by_css_selector('a[id="searchBtn1"]').send_keys("\n")
+                driver.find_element(By.CSS_SELECTOR, 'a[id="searchBtn1"]').send_keys("\n")
                 driver.implicitly_wait(20)
-                driver.find_element_by_xpath('//*[@id="tab1"]/section[2]/table/tbody/tr/td[2]/a').click()
+                driver.find_element(By.XPATH, '//*[@id="tab1"]/section[2]/table/tbody/tr/td[2]/a').click()
 
                 # 메인 창으로 전환
                 driver.switch_to.window(main)
-                driver.find_element_by_xpath(self.data_btn).click()
+                driver.find_element(By.XPATH, self.data_btn).click()
 
                 # iframe : iframe 영역
-                iframe = driver.find_element_by_class_name('olapViewFrame')
+                iframe = driver.find_element(By.CLASS_NAME, 'olapViewFrame')
                 driver.switch_to.frame(iframe)
                 # driver.switch_to.frame
                 driver.implicitly_wait(20)
@@ -72,7 +77,7 @@ class DiagBhvInfoCrawler(object):
                     wait = WebDriverWait(driver, 20)
                     ## 진료년월 라디오 버튼
                     radio = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ext-gen1645"]/table/tbody/tr/td/div[2]/table/tbody/tr/td/ul/li[2]/label/input')))
-                    radio = driver.find_element_by_xpath('//*[@id="ext-gen1645"]/table/tbody/tr/td/div[2]/table/tbody/tr/td/ul/li[2]/label/input')
+                    radio = driver.find_element(By.XPATH, '//*[@id="ext-gen1645"]/table/tbody/tr/td/div[2]/table/tbody/tr/td/ul/li[2]/label/input')
                     driver.execute_script("arguments[0].click();", radio)
 
                     ### 검색 시작 날짜 선택
@@ -88,17 +93,18 @@ class DiagBhvInfoCrawler(object):
 
                     ## 달력에서 년도 선택
                     # //*[@id="monthpicker_03292706759513271"]/div/select/option[9]
-                    from_year_xpath = f"//*[@id='{mpid[0].attrs['id']}']/div/select/option[9]"
+                    from_year_xpath = f"//*[@id='{mpid[0].attrs['id']}']/div/select/option[11]"
                     from_year = wait.until(EC.visibility_of_element_located((By.XPATH, from_year_xpath)))
                     from_year.click()
 
                     ## 달력에서 월 선택
-                    from_month_xpath = f"//*[@id='{mpid[0].attrs['id']}']/table/tbody/tr[1]/td[1]"
+                    from_month_xpath = f"//*[@id='{mpid[0].attrs['id']}']/table/tbody/tr[4]/td[3]"
                     from_month = wait.until(EC.visibility_of_element_located((By.XPATH, from_month_xpath)))
                     from_month.click()
 
+                    time.sleep(5)
                     radio = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ext-gen1645"]/table/tbody/tr/td/div[2]/table/tbody/tr/td/ul/li[2]/label/input')))
-                    radio = driver.find_element_by_xpath('//*[@id="ext-gen1645"]/table/tbody/tr/td/div[2]/table/tbody/tr/td/ul/li[2]/label/input')
+                    radio = driver.find_element(By.XPATH, '//*[@id="ext-gen1645"]/table/tbody/tr/td/div[2]/table/tbody/tr/td/ul/li[2]/label/input')
                     driver.execute_script("arguments[0].click();", radio)
 
                     ### 검색 시작 날짜 선택
@@ -114,16 +120,16 @@ class DiagBhvInfoCrawler(object):
 
                     ## 달력에서 년도 선택
                     # //*[@id="monthpicker_03292706759513271"]/div/select/option[9]
-                    from_year_xpath = f"//*[@id='{mpid[0].attrs['id']}']/div/select/option[9]"
+                    from_year_xpath = f"//*[@id='{mpid[0].attrs['id']}']/div/select/option[11]"
                     from_year = wait.until(EC.visibility_of_element_located((By.XPATH, from_year_xpath)))
                     from_year.click()
 
                     ## 달력에서 월 선택
-                    from_month_xpath = f"//*[@id='{mpid[0].attrs['id']}']/table/tbody/tr[1]/td[1]"
+                    from_month_xpath = f"//*[@id='{mpid[0].attrs['id']}']/table/tbody/tr[4]/td[3]"
                     from_month = wait.until(EC.visibility_of_element_located((By.XPATH, from_month_xpath)))
                     from_month.click()
 
-
+                    time.sleep(5)
                     ### 검색 끝 날짜 선택
                     to_date = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ext-gen1645']/table/tbody/tr/td/div[4]/div/input[2]")))
                     to_date.click()
@@ -141,7 +147,7 @@ class DiagBhvInfoCrawler(object):
                     to_month.click()
 
                     ## 조회 버튼
-                    search_btn = driver.find_element_by_class_name("dt-btn-search")
+                    search_btn = driver.find_element(By.CLASS_NAME, "dt-btn-search")
                     driver.execute_script("arguments[0].click();", search_btn)
                     driver.implicitly_wait(30)
                     ## 데이터가 로드될 때 까지 기다리기
@@ -153,7 +159,7 @@ class DiagBhvInfoCrawler(object):
                         driver.implicitly_wait(30)
                     ## 조회된 데이터가 없는 경우
                     except Exception as e:
-                        print("조회된 데이터가 없는 경우 except")
+                        print(f" {code}: 조회된 데이터가 없는 경우 except")
                         print(e)
                         # base_dir = Path(__file__).parent
                         # failpath = os.path.join(base_dir, 'failedfiles')
@@ -166,7 +172,7 @@ class DiagBhvInfoCrawler(object):
                     # download_excel : 엑셀 다운로드 하는 버튼
 
                     print("excel downloads")
-                    download_excel = driver.find_element_by_xpath('//*[@id="panel-1184-body"]/div[2]/div[1]/div[1]/div[2]/div[1]')
+                    download_excel = driver.find_element(By.XPATH, '//*[@id="panel-1184-body"]/div[2]/div[1]/div[1]/div[2]/div[1]')
                     driver.execute_script("arguments[0].click();", download_excel)
 
                     ## csv 파일명
@@ -195,11 +201,12 @@ class DiagBhvInfoCrawler(object):
 
                     # global failed_lst
                     if code in failed_lst:
+                        print(f"{code}")
                         failed_lst.remove(code)
                     # driver.close()
                     continue
                 except Exception as e:
-                    print (e)
+                    print (f"{code}: {e}")
                     # driver.close()
                     continue
             except Exception as e:
@@ -218,18 +225,22 @@ if __name__ == "__main__":
     by_institution = '1_진료행위요양기관그룹별현황'
     by_location = '1_진료행위요양기관소재지별현황'
     url = 'http://opendata.hira.or.kr/op/opc/olapDiagBhvInfo.do'
-    directory = os.path.join(os.getcwd(), 'downloads')
+    directory = os.path.join(os.getcwd(), 'csv_file')
     print(directory)
     # directory = 'C:\\Users\\sm\\Downloads\\'
 
     # mdfeeCd_lst : 진료행위 코드 리스트
-    df = pd.read_excel('csv_file/최신edicode7차.xlsx')
+    df = pd.read_excel('csv_file/수가코드모수new12월.xlsx')
     print(df)
     mdfeeCd_lst = df['EDICODE']
+    global failed_lst
+    failed_lst = []
+    global count
+    count = 0
 
     # url, md_code, data_btn , count , directory , by):
     ## crawler_ins1 : 요양기관종별 데이터 크롤링
     print ('crawling Data ..............')
-    crawler_ins1 = DiagBhvInfoCrawler(url, mdfeeCd_lst, institution_btn, directory , by_institution)
+    crawler_ins1 = OpendataCrawler(url, mdfeeCd_lst, institution_btn, directory , by_institution)
     # crawler_ins1 = OpendataCrawler(url, mdfeeCd_lst, location_btn, directory , by_location)
     crawler_ins1.crawl_data()
